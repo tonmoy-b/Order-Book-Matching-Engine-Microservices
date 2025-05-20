4a2715ba8c9a83e3e2e287d63807617fc946ae40
Tonmoy Bhattacharya <ultraton_16@yahoo.com>
2025-05-20 21:25:50 +0530

    docker-compose-01.yml added

--- orderfront/src/main/java/com/tbhatta/orderfront/kafka/KafkaProducer.java
+++ orderfront/src/main/java/com/tbhatta/orderfront/kafka/KafkaProducer.java
@@ -3,6 +3,7 @@ package com.tbhatta.orderfront.kafka;
 import ch.qos.logback.core.encoder.EchoEncoder;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
 import com.tbhatta.orderfront.entity.OrderItem;
@@ -12,6 +13,7 @@ import com.tbhatta.protos.OrderItemEvent;
 public class KafkaProducer {
 
     private static final Logger log = LoggerFactory.getLogger(KafkaProducer.class);
+
     private final KafkaTemplate<String, byte[]> kafkaTemplate;
 
     public KafkaProducer(KafkaTemplate<String, byte[]> kafkaTemplate) {
