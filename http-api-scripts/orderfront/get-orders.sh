120213178c6c9bdfd69f7333955b96a4f94205e6
Tonmoy Bhattacharya <ultraton_16@yahoo.com>
2025-06-17 10:56:38 +0530

    moved  scripts into the scripts folder

--- http-api-scripts/orderfront/get-orders.sh
+++
@@ -1,5 +0,0 @@
-curl -X POST localhost:4000/orders \
-   -H 'Content-Type: application/json' \
-   -d '{"login":"my_login","password":"my_password"}'
-
-   curl -X POST localhost:4000/orders -H 'Content-Type: application/json' -d '{"login":"my_login","password":"my_password"}'
